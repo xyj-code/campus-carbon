3ea6433c140d19c5c4c2d7dac637a9d27a02344a
xyj-code <3583003159@qq.com>
2026-03-31 15:23:52 +0800

    Delete campus-carbon-main directory

--- campus-carbon-main/src/main/java/com/campus/carbon/service/PointsService.java
+++
@@ -1,9 +0,0 @@
-package com.campus.carbon.service;
-
-import java.util.Map;
-
-public interface PointsService {
-    void awardStepPoints(String username, int deltaSteps);
-    Map<String, Object> getProfile(String username);
-    Map<String, Object> getRecords(String username, String type, int page, int size);
-}
--- campus-carbon-main/src/main/java/com/campus/carbon/service/StudentService.java
+++
@@ -1,7 +0,0 @@
-package com.campus.carbon.service;
-
-import com.campus.carbon.model.Student;
-
-public interface StudentService {
-    Student login(String username, String password);
-}
